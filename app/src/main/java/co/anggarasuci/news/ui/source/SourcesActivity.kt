a6c0f8db8ba7d6aa57abb09d5dfd644675273210
Anggara Suci Nugraha <anggarasucinugraha@gmail.com>
2021-01-07 14:59:02 +0700

    styling and ui

--- app/src/main/java/co/anggarasuci/news/ui/source/SourcesActivity.kt
+++ app/src/main/java/co/anggarasuci/news/ui/source/SourcesActivity.kt
@@ -1,10 +1,18 @@
 package co.anggarasuci.news.ui.source
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.anggarasuci.news.R
 import co.anggarasuci.news.base.BaseActivity
+import com.afollestad.materialdialogs.utils.MDUtil.getStringArray
+import kotlinx.android.synthetic.main.sources_activity.*
 
 class SourcesActivity: BaseActivity() {
+
+    private val _categoryAdapter by lazy {
+        CategoryAdapter { onCategoryItemClick(it) }
+    }
 
     override fun getScreenName() = javaClass.simpleName
 
@@ -15,7 +23,7 @@ class SourcesActivity: BaseActivity() {
     }
 
     override fun setupView() {
-
+        initCategoryAdapter()
     }
 
     override fun setupViewEvent() {
@@ -23,6 +31,19 @@ class SourcesActivity: BaseActivity() {
     }
 
     override fun subscribeState() {
+
+    }
+
+    private fun initCategoryAdapter() {
+        val mLayoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        tab_rv?.apply {
+            layoutManager = mLayoutManager
+            adapter = _categoryAdapter
+        }
+        _categoryAdapter.setData(getStringArray(R.array.categories_array).toList())
+    }
+
+    private fun onCategoryItemClick(category: String) {
 
     }
 }
