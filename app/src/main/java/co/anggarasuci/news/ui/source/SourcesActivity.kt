f48093a28439f1992f02989b6b020af5df4606ad
Anggara Suci Nugraha <anggarasucinugraha@gmail.com>
2021-01-07 15:35:59 +0700

    integration sources

--- app/src/main/java/co/anggarasuci/news/ui/source/SourcesActivity.kt
+++ app/src/main/java/co/anggarasuci/news/ui/source/SourcesActivity.kt
@@ -5,14 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.anggarasuci.news.R
 import co.anggarasuci.news.base.BaseActivity
+import co.anggarasuci.news.data.model.Source
+import co.anggarasuci.news.util.RecyclerViewLoadMoreScroll
 import com.afollestad.materialdialogs.utils.MDUtil.getStringArray
 import kotlinx.android.synthetic.main.sources_activity.*
+import org.koin.android.viewmodel.ext.android.viewModel
+import co.anggarasuci.news.ui.source.SourcesViewModel.State
+import co.anggarasuci.news.ui.source.SourcesViewModel.Event
+import co.anggarasuci.news.util.subscribeSingleLiveEvent
 
 class SourcesActivity: BaseActivity() {
+    private val _viewModel: SourcesViewModel by viewModel()
+    private val _categoryAdapter by lazy { CategoryAdapter { onCategoryItemClick(it) } }
+    private lateinit var _loadMoreScroll: RecyclerViewLoadMoreScroll
+    private val _sourceAdapter by lazy { SourcesAdapter { onSourceItemClick(it) } }
 
-    private val _categoryAdapter by lazy {
-        CategoryAdapter { onCategoryItemClick(it) }
-    }
 
     override fun getScreenName() = javaClass.simpleName
 
@@ -20,10 +27,12 @@ class SourcesActivity: BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _viewModel.onEventReceived(Event.OnCreate)
     }
 
     override fun setupView() {
         initCategoryAdapter()
+        initSourceAdapter()
     }
 
     override fun setupViewEvent() {
@@ -31,7 +40,18 @@ class SourcesActivity: BaseActivity() {
     }
 
     override fun subscribeState() {
+        subscribeSingleLiveEvent(_viewModel.state) {
+            when (it) {
+                is State.Error -> { }
+                is State.ShowData -> { }
+                is State.ShowLoading -> { }
+            }
+        }
+    }
 
+    private fun onShowData(data: List<Source>) {
+        _loadMoreScroll.setLoaded()
+        _sourceAdapter.setNewData(data)
     }
 
     private fun initCategoryAdapter() {
@@ -43,7 +63,22 @@ class SourcesActivity: BaseActivity() {
         _categoryAdapter.setData(getStringArray(R.array.categories_array).toList())
     }
 
+    private fun initSourceAdapter() {
+        val mLayoutManager = LinearLayoutManager(this)
+        _loadMoreScroll = RecyclerViewLoadMoreScroll(mLayoutManager) {  }
+        rv?.apply {
+            layoutManager = mLayoutManager
+            adapter = _sourceAdapter
+            setHasFixedSize(true)
+            addOnScrollListener(_loadMoreScroll)
+        }
+    }
+
     private fun onCategoryItemClick(category: String) {
+        _viewModel.onEventReceived(Event.OnItemCategoryClick(category))
+    }
+
+    private fun onSourceItemClick(id: String) {
 
     }
 }
---
+++ app/src/main/java/co/anggarasuci/news/ui/source/SourcesAdapter.kt
@@ -0,0 +1,62 @@
+package co.anggarasuci.news.ui.source
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import co.anggarasuci.news.R
+import co.anggarasuci.news.data.model.Source
+import kotlinx.android.synthetic.main.item_source.view.*
+
+class SourcesAdapter(
+    private val onItemClicked: (id: String) -> Unit
+) : RecyclerView.Adapter<SourcesAdapter.ViewHolder>() {
+
+    private var _items = mutableListOf<Source>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_source, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return _items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder?.apply {
+            val model = _items[adapterPosition]
+            name?.text = model.name
+            description?.text = model.description
+            categoryName?.text = model.category
+            container?.setOnClickListener { onItemClicked(model.id) }
+        }
+    }
+
+    fun setNewData(sources: List<Source>) {
+        clear()
+        addData(sources)
+    }
+
+    fun addData(sources: List<Source>?) {
+        sources?.let {
+            _items.addAll(it)
+            notifyItemInserted(_items.size)
+        }
+    }
+
+    fun clear() {
+        _items.clear()
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.name_txt
+        val description: TextView = view.description_txt
+        val categoryName: TextView = view.category_txt
+        val container: ConstraintLayout = view.container
+    }
+}
